e0e337548ac2168ee8937323bf04556a35a84813
Carlos Tolosa <carloswtolosa@gmail.com>
2025-06-05 20:34:11 -0300

    Clase Pedido

---
+++ proyectoECommerce/src/main/java/Pedido.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Pedido {
+    private ArrayList<LineaPedido> lineasPedidos = new ArrayList<>();
+
+    public void agregarLinea(LineaPedido lineaPedido){
+        lineasPedidos.add(lineaPedido);
+        System.out.println("Agregado al carrito");
+    }
+
+    public double calcularTotal(){
+        double total = 0;
+        for (LineaPedido lp : lineasPedidos){
+            total = total + lp.getSubtotal();
+        }
+        return total;
+    }
+
+    public void mostrarPedido(){
+        System.out.println("Detalles del pedido: ");
+        for (LineaPedido lp : lineasPedidos){
+            lp.mostrarLineaPedido();
+        }
+        System.out.println("Total: $" + calcularTotal());
+    }
+}
