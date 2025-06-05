085ceaf6f3bf445a4244cfe0c9ef3a82521a30fc
Carlos Tolosa <carloswtolosa@gmail.com>
2025-06-05 19:09:15 -0300

    Clase Producto

---
+++ proyectoECommerce/src/main/java/Producto.java
@@ -0,0 +1,36 @@
+import java.security.PublicKey;
+import java.util.*;
+
+public class Producto {
+    private static int contador = 0;
+    private int id;
+    private String nombre;
+    private double precio;
+    private int cantStock;
+
+    public Producto(String nombre, double precio, int cantStock){
+        this.id = ++contador;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.cantStock = cantStock;
+    }
+
+    public void setId(){ this.id = ++contador; }
+    public int getId(){ return  id; }
+
+    public void setNombre(String nombre){ this.nombre = nombre; }
+    public String getNombre(){ return nombre; }
+
+    public void setPrecio(double precio){ this.precio = precio; }
+    public double getPrecio(){ return precio; }
+
+    public void setCantStock(int cantStock){ this.cantStock = cantStock; }
+    public int getCantStock(){return cantStock; }
+
+    public void mostrarListaProducto(){
+        System.out.println("ID: " + id);
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Precio: $" + precio);
+        System.out.println("Stock: " + cantStock + " unidades");
+    }
+}
